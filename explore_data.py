8826a09382aa860fbb5c25c5772caa694a7d5aa8
sdonovan001 <scott@the-donovans.org>
2025-03-25 10:48:47 -0500

    refactoring...

--- explore_data.py
+++ explore_data.py
@@ -1,29 +1,30 @@
-import io
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset = pd.read_csv("./datasets/taxi-data.csv")
-trimmed_ds = dataset[['TRIP_MILES', 'TRIP_MINUTES', 'FARE', 'COMPANY', 'TIP_RATE']]
+
+dataframe = pd.read_csv("./datasets/taxi-data.csv")
+trimmed_df = dataframe[['TRIP_MILES', 'TRIP_MINUTES', 'FARE', 'COMPANY', 'TIP_RATE']]
 
 print("\n------------------------------ Columns of Interest -----------------------------------")
-print(trimmed_ds.info())
+print(trimmed_df.info())
 
 print("\n---------------------------------- Example Data --------------------------------------")
-print(trimmed_ds.head(200))
+print(trimmed_df.head(200))
 
 print("\n--------------------------------- Data Statistics ------------------------------------")
-print(trimmed_ds.describe(include='all'))
+print(trimmed_df.describe(include='all'))
 
 print("\n--------------------------- Correlation Between Features -----------------------------")
-print(trimmed_ds.corr(numeric_only=True))
+print(trimmed_df.corr(numeric_only=True))
 
-sns.pairplot(trimmed_ds, x_vars=["TRIP_MILES", "TRIP_MINUTES"], y_vars=["FARE"])
+print("\n----------------------------------- Plot Data ----------------------------------------")
+sns.pairplot(trimmed_df, x_vars=["TRIP_MILES", "TRIP_MINUTES"], y_vars=["FARE"])
 plt.show()
 
 print("\n----------------------- Creating Training / Testing Datasets -------------------------")
-training_ds, validation_ds = train_test_split(trimmed_ds, test_size=0.2, random_state=42) 
-training_ds.to_csv('./datasets/training-data.csv', index=False)
-validation_ds.to_csv('./datasets/validation-data.csv', index=False)
+training_df, validation_df = train_test_split(trimmed_df, test_size=0.2, random_state=42) 
+training_df.to_csv('./datasets/training-data.csv', index=False)
+validation_df.to_csv('./datasets/validation-data.csv', index=False)
